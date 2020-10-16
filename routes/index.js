06e568cc151356457021b09ca64dc8d6f947e708
Marcin Napiorkowski <napiorkowski.marcin.gmail.com>
2020-10-16 13:11:53 +0200

    create dashboard, next: edit stories btn

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuth, ensureGuest } = require("../middleware/auth.js");
+const Story = require("../models/Story");
 
 //Login-landing page
 //route Get/
@@ -12,10 +13,17 @@ router.get("/", ensureGuest, (req, res) => {
 
 //Dashboard
 //Get/dashboard
-router.get("/dashboard", ensureAuth, (req, res) => {
-  res.render("dashboard", {
-    name: req.user.firstName,
-  });
+router.get("/dashboard", ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({ user: req.user.id }).lean();
+    res.render("dashboard", {
+      name: req.user.firstName,
+      stories,
+    });
+  } catch (err) {
+    console.log(err);
+    res.render("/error/500");
+  }
 });
 
 module.exports = router;
